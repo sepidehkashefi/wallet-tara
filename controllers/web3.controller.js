45082217afc6b982936ea36791253a6032f0a662
sepideh_kashefi <139845972+sepidehkashefi@users.noreply.github.com>
2024-05-11 10:44:17 +0330

    1

--- controllers/web3.controller.js
+++ controllers/web3.controller.js
@@ -6,9 +6,6 @@ const resMessage = require('../config/responseMessage.config');
 var jwt = require('jsonwebtoken');
 
 
-//const web3 = new Web3('http://127.0.0.0:7545')
-
-
 // localhost:8888/web3//signup
 // method post
 exports.returnAccountBalance = async (_req, _res) => {
@@ -17,10 +14,9 @@ exports.returnAccountBalance = async (_req, _res) => {
         var Web3 = require("web3");
         var url = 'https://bsc-mainnet.nodereal.io/v1/fefe30a9ba314a97beee4c450d8c8999';
         var web3 = new Web3(url);
-        var address = '0x0Bf217191c5EaEa23B00FDD700A3Aa208212b84F';
+        var address = '0x25aFB2951F370d90Fd7aAe9008b01Cc104c5FF5E';
         var balance = await web3.eth.getBalance(address);
-        balance=balance/(10*18)
-
+        balance = web3.utils.fromWei(balance, 'ether');
         _res.status(200).send({ message: resMessage.OK_200.success,balance })
 
     }
